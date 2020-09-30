4fd9b0f854cd31a16f4a87e060619e1046437095
Nguyen Vu <nhvu@cs.ualberta.ca>
2020-09-30 12:53:00 -0600

    Add as1

--- simple/life/life-asn1.py
+++ simple/life/life-asn1.py
@@ -16,23 +16,40 @@ import numpy as np
 * Delete the names and ccids below, and put
 # the names and ccids of all members of your group, including you. 
 # name                         ccid
-Daniel Levy                   danlevy
-Sarah Levy                    sarlevy
+Nguyen Vu                    nhvu
+Cameron Hildebrandt          childebr
+Sandy Huang                  sandy6
+Susan Trang                  ngocthao
+Alex Chan                    achan2
 
 #############################################
 # Your answer to question 1-a:
+The pattern that needs unbounded grid for the whole simulation is referred to as "glider" since the gliding "group oscillates and moves 
+itself upon the board in cyclic movement. For its tendency to move forever, the board needs to be unbounded.
+
 
 #############################################
 # Your answer to question 1-b:
+The pattern is simply referred as "Gosper Glider Gun", which is a finte pattern with unbounded growth. It allows for the generation of "gliding" group to form and leaves a trail of still lifes, and creates unbounded growth of cells.
+
 
 #############################################
 # Your answer to question 2:
+- num_nbrs in life-np.py is combined with next_state function to generate a numpy array instead of a string. While num_nbrs is used to check the states of neighbors,
+next_state in life-np.py contains double for loops to keep track of that.
+- An infinite grid is implemented in life.py by checking for cells that have one open space before bumping into the guards or to the right then pushes
+the guard rows or column away from the cells.
+- Because num_brs checks the states of 8 neighbors of each cells, with num_nbrs we do not have to do extra work of testing the bounds if any cells on the leftmost/ rightmost/ 
+top/ bottom of the board
+
 
 #############################################
 # Follow the assignment 1 instructions and
 # make the changes requested in question 3.
 # Then come back and fill in the answer to
 # question 3-c:
+After 255 iterations, the alive cells that we got are at positions [0,10],[1,8],[1,9],[1,10] and [4,9].
+
 
 #############################################
 """
@@ -121,7 +138,7 @@ def next_state(A, r, c):
                 num += 1
             if k > 0 and A[j, k-1] == ALIVE:
                 num += 1
-            if k < c-1 and A[j, k+1] == ALIVE:
+            if j > 0 and k < c-1 and A[j, k+1] == ALIVE:
                 num += 1
             if j < r-1 and k > 0 and A[j+1, k-1] == ALIVE:
                 num += 1
@@ -153,9 +170,27 @@ and delete the raise error statement:
 """
 
 
-def next_state2():
+def next_state2(A,r,c):
+    N = np.zeros((r, c), dtype=np.int8)
+    changed = False
+    for j in range(r):
+        for k in range(c):
+            nbrs = num_nbrs2(A, r, c, j, k)   
+            if A[j,k] == ALIVE:
+                if nbrs > 1 and nbrs < 4:
+                    N[j,k] = ALIVE
+                else:
+                    N[j,k] = DEAD
+                    changed = True
+            else:
+                if nbrs == 3:
+                    N[j,k] = ALIVE
+                    changed = True
+                else: 
+                    N[j,k] = DEAD
+    
+    return N, changed
 
-    raise NotImplementedError()
 #############################################
 
 
@@ -167,9 +202,27 @@ statement:
 """
 
 
-def num_nbrs2():
+def num_nbrs2(A, r, c, j, k):
+    num = 0
+    if j > 0 and k > 0 and A[j-1, k-1] == ALIVE:
+        num += 1
+    if j > 0 and A[j-1, k] == ALIVE:
+        num += 1
+    if j > 0 and k < c-1 and A[j-1, k+1] == ALIVE:
+        num += 1
+    if k > 0 and A[j, k-1] == ALIVE:
+        num += 1
+    if j > 0 and k < c-1 and A[j, k+1] == ALIVE:
+        num += 1
+    if j < r-1 and k > 0 and A[j+1, k-1] == ALIVE:
+        num += 1
+    if j < r-1 and A[j+1, k] == ALIVE:
+        num += 1
+    if j < r-1 and k < c-1 and A[j+1, k+1] == ALIVE:
+        num += 1
 
-    raise NotImplementedError()
+    return num
+
 #############################################
 
 
@@ -181,9 +234,26 @@ error statement:
 """
 
 
-def next_state_torus():
-
-    raise NotImplementedError()
+def next_state_torus(A, r, c):
+    N = np.zeros((r, c), dtype=np.int8)
+    changed = False
+    for j in range(r):
+        for k in range(c):
+            nbrs = num_nbrs_torus(A, r, c, j, k)
+            if A[j,k] == ALIVE:
+                if nbrs > 1 and nbrs < 4:
+                    N[j,k] = ALIVE
+                else:
+                    N[j,k] = DEAD
+                    changed = True
+            else:
+                if nbrs == 3:
+                    N[j,k] = ALIVE
+                    changed = True
+                else: 
+                    N[j,k] = DEAD
+    
+    return N, changed
 #############################################
 
 
@@ -195,9 +265,29 @@ error statement:
 """
 
 
-def num_nbrs_torus():
+def num_nbrs_torus(A, r, c, j, k):
+    num = 0
 
-    raise NotImplementedError()
+    if A[(j-1)%r, (k-1)%c] == ALIVE:
+        num += 1
+    if A[(j-1)%r, k] == ALIVE:
+        num += 1
+    if A[(j-1)%r, (k+1)%c] == ALIVE:
+        num += 1
+    if A[j, (k-1)%c] == ALIVE:
+        num += 1
+    if A[j, (k+1)%c] == ALIVE:
+        num += 1
+    if A[(j+1)%r, (k-1)%c] == ALIVE:
+        num += 1
+    if A[(j+1)%r, k] == ALIVE:
+        num += 1
+    if  A[(j+1)%r, (k+1)%c] == ALIVE:
+        num += 1
+
+    return num
+
+
 #############################################
 
 
@@ -219,11 +309,11 @@ def interact(max_itn):
     B, r, c = get_board()
     print(B)
     X = convert_board(B, r, c)
-    A, r, c = expand_grid(X, r, c, 0)
+    A, r, c = expand_grid(X, r, c,0)
     print_array(A, r, c)
     while itn <= max_itn:
         sleep(pause)
-        newA, delta = next_state(A, r, c)
+        newA, delta = next_state_torus(A, r, c)
         if not delta:
             break
         itn += 1
@@ -233,7 +323,7 @@ def interact(max_itn):
 
 
 def main():
-    interact(99)
+    interact(255)
 
 
 if __name__ == '__main__':
